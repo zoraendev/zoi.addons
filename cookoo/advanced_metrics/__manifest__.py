197958df1c64f2cff50d2ccbe6a8ddea498cf600
zoraendev <106628009+wsalguero@users.noreply.github.com>
2026-03-28 18:56:36 -0600

    Update website URL in manifest and enhance dashboard styles

--- cookoo/advanced_metrics/__manifest__.py
+++ cookoo/advanced_metrics/__manifest__.py
@@ -9,7 +9,7 @@ Long description of module's purpose
     """,
 
     'author': "My Company",
-    'website': "https://www.yourcompany.com",
+    'website': "https://www.zoraen.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
