2238f7b58363ea12c0ab6a233b7c7b39dc04401f
wsalguero <risefuncdev@gmail.com>
2026-05-28 21:36:35 -0600

    dev

--- zrn_prodigyn/models/__init__.py
+++ zrn_prodigyn/models/__init__.py
@@ -4,4 +4,5 @@ from . import models
 from . import mfg_plan
 from . import production_planning_wizard
 from . import purchase_planning_wizard
+from . import reporting_analysis
 from . import res_config_settings
